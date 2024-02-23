fa726e7da5225b390d7f4a65daddffb50ca45bb5
taiyakisusume <157872668+taiyakisusume@users.noreply.github.com>
2024-02-23 21:32:15 +0900

    feat: add test pages

---
+++ frontend/pages/about.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <h1>情報</h1>
+    <hr />
+    <NuxtLink to="/">トップページに戻る</NuxtLink>
+    <br />
+    <NuxtLink to="https://github.com/taiyakisusume/univ_blog">このブログのリポジトリ(public)</NuxtLink>
+    <p>ここに詳細が表示される</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ frontend/pages/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <h1>トップページ</h1>
+    <hr />
+    <NuxtLink to="/about">このブログについての情報はこちら</NuxtLink>
+    <br />
+    <p>ここに記事一覧が表示される予定</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
